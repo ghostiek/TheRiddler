7d7338b76ec900c3fbb50210ae81f6dce9291673
Patrick S. Tawil <patricktawil1@hotmail.com>
2019-03-21 15:39:56 +0800

    Create Express.py

---
+++ 2019-03-01/Express.py
@@ -0,0 +1,22 @@
+#2
+
+total = 0
+n = 10000
+for i in range(0,n):
+    cards = np.arange(0,144) #0-143
+    counter = 0
+    numberofunique = 0
+    cardsowned = []
+    while True:
+        counter = counter + 1
+        cardbought = np.random.choice(cards)
+        if not cardbought in cardsowned:
+            numberofunique = numberofunique + 1
+            cardsowned.append(cardbought)
+            if numberofunique == 144:
+                break
+    total = total + counter
+    if i %1000 == 0:
+        print(f"{100*i/n}%")
+    
+print(total*5/n)
